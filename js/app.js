b41bdf323ee01f393223560e4427aef9ea6933c3
kvng777 <kjangamboa@hotmail.com>
2020-12-23 00:54:01 +0800

    Html, Css, JS updates
    1. Manually added in images to display
    2. Added a header section
    3. Styling applied as per mock up
    4. Added plugins for JS lightbox and searchfilter

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 baguetteBox.run('.gallery');
+const search = new Filter('search', 'data-caption');
 
-const main = document.querySelector('main');
-let html ='';
 
-for(let i=1; i <=12; i++ ) {
-    html += `
-    <div>
-        <a href="photos/${i}.jpg">
-            <img src="photos/thumbnails/${i}.jpg" alt="Photo display ${i}">
-        </a>
-    </div>
-`;
-}
+//Code below is to display the images with loop.
 
-main.innerHTML = html;
+
+// const main = document.querySelector('main');
+// let html ='';
+
+// for(let i=1; i <=12; i++ ) {
+//     html += `
+//     <div>
+//         <a href="photos/${i}.jpg">
+//             <img src="photos/thumbnails/${i}.jpg" alt="Photo display ${i}">
+//         </a>
+//     </div>
+// `;
+// }
+
+// main.innerHTML = html;
